e9222d8ceaed9277310005198fcebac2cabf0347
Marius Tolzmann <tolzmann@molgen.mpg.de>
2015-08-26 18:15:54 +0200

    mxqsub: Use mx_strtobytes() to parse --memory option

--- mxqsub.c
+++ mxqsub.c
@@ -775,8 +775,13 @@ int main(int argc, char *argv[])
 
             case 'm':
                 if (mx_strtou64(optctl.optarg, &arg_memory) < 0) {
-                    mx_log_crit("--memory '%s': %m", optctl.optarg);
-                    exit(EX_CONFIG);
+                    unsigned long long int bytes;
+
+                    if(mx_strtobytes(optctl.optarg, &bytes) < 0) {
+                        mx_log_crit("--memory '%s': %m", optctl.optarg);
+                        exit(EX_CONFIG);
+                    }
+                    arg_memory = bytes/1024/1024;
                 }
                 break;
 
