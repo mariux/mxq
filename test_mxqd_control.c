a28a7c793dd1de347a9e1869e83e7e13d1055d59
Marius Tolzmann <tolzmann@molgen.mpg.de>
2015-11-03 13:01:50 +0100

    test_mxqd_control: Init server structure

--- test_mxqd_control.c
+++ test_mxqd_control.c
@@ -3,8 +3,30 @@
 
 #include <assert.h>
 
+#include "mxqd.h"
+
+#define MEMORY_TOTAL 20480
+#define MEMORY_MAX_PER_SLOT 2048
+#define SLOTS 10
+
+void __init_server(struct mxq_server *server)
+{
+    server->memory_total        = MEMORY_TOTAL;
+    server->memory_max_per_slot = MEMORY_MAX_PER_SLOT;
+    server->slots               = SLOTS;
+    server->memory_avg_per_slot = MEMORY_TOTAL / SLOTS;
+}
+
+
 static void test_mxqd_control(void)
 {
+    struct mxq_server _server = { 0 };
+    struct mxq_server *server;
+
+    server = &_server;
+
+    __init_server(server);
+
     assert(1);
 }
 
